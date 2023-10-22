2a7656f91526168bce0461ed5d15ea3b5d538448
Temux Ocit <ocritocrit@gmail.com>
2023-10-22 12:30:26 +0700

    add logo to nav

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,8 +1,31 @@
 'use client'
 
+import Logo from "./Logo"
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div
+      className="
+      flex
+      gap-10
+      lg:gap-20
+      justify-between
+      "
+    >
+      <div
+      className="
+        flex
+        gap-4
+        items-center
+        flex-shrink-0
+        "
+      >
+        <button></button>
+        <div className="h-6">
+          <Logo/>
+        </div>
+      </div>
+    </div>
   )
 }
 
