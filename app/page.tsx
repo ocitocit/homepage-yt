79d65ed0cabd67e3fa6d1bce322e5da54e8c479c
Abdul Rosyid <ocritocrit@gmail.com>
2023-10-24 06:58:51 +0700

    add categorypills

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,22 @@
+import CategoryPills from '@/components/CategoryPills';
+import { categories } from '@/libs/data';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <main
+      className="
+        grid 
+        grid-cols-[auto,1fr]
+        flex-grow-1
+        overflow-auto
+      "
+    >
+      <div>SIDEBAR</div>
+      <div className="sticky top-0 z-10 bg-white pb-4">
+        <CategoryPills categories={categories} />
+      </div>
+    </main>
+  );
 };
 
 export default Home;
