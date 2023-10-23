259e74b4bfd57ba52ecb231f899faa3cf07dd4c3
Abdul Rosyid <ocritocrit@gmail.com>
2023-10-24 06:55:42 +0700

    add local data

---
+++ libs/data.ts
@@ -0,0 +1,19 @@
+export const categories = [
+  'All',
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'Next.js 13',
+  'Computer Programming',
+  'Gaming',
+  'Live',
+  'Podcast',
+  'Coding',
+  'Frontend Web Development',
+  'Backend Web Development',
+  'Web Development',
+  'Comedy',
+  'Recently Upload',
+  'Watched',
+  'New to you'
+];
