5a89a59ee64e11fb2792eb69ecb266bca87e0c0b
Abdul Rosyid <ocritocrit@gmail.com>
2023-10-27 23:38:32 +0700

    feat: video play while mouse enter

--- components/VideoGridItem.tsx
+++ components/VideoGridItem.tsx
@@ -4,6 +4,7 @@ import { formatDuration } from '@/libs/formatDuration';
 import { formatTimeAgo } from '@/libs/formatTimeAgo';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import { useEffect, useRef, useState } from 'react';
 
 interface VideoGridItemProps {
   id: string;
@@ -36,8 +37,25 @@ const VideoGridItem: React.FC<VideoGridItemProps> = ({
     notation: 'compact'
   });
 
+  const [isVideoPlaying, setisVideoPlaying] = useState(false);
+  const videoRef = useRef<HTMLVideoElement>(null);
+  useEffect(() => {
+    if (videoRef.current == null) return;
+
+    if (isVideoPlaying) {
+      videoRef.current.currentTime = 0;
+      videoRef.current.play();
+    } else {
+      videoRef.current.pause();
+    }
+  }, [isVideoPlaying]);
+
   return (
-    <div className="flex flex-col gap-2">
+    <div
+      onMouseEnter={() => setisVideoPlaying(true)}
+      onMouseLeave={() => setisVideoPlaying(false)}
+      className="flex flex-col gap-2"
+    >
       <div
         onClick={() => router.push(`/watch?v=${id}`)}
         className="relative aspect-video"
@@ -45,13 +63,16 @@ const VideoGridItem: React.FC<VideoGridItemProps> = ({
         <Image
           src={thumbnailUrl}
           fill
-          className="
+          className={`
             block
             w-full
             h-full
             object-cover
-            rounded-xl
-          "
+            cursor-pointer
+            duration-200
+            transition-[border-radius] 
+            ${isVideoPlaying ? 'rounded-none' : 'rounded-xl'}
+          `}
           alt={title}
         />
 
@@ -69,6 +90,22 @@ const VideoGridItem: React.FC<VideoGridItemProps> = ({
         >
           {formatDuration(duration)}
         </div>
+        <video
+          className={`
+            block
+            h-full
+            object-cover
+            absolute
+            inset-0
+            transition-opacity
+            duration-200
+            ${isVideoPlaying ? 'opacity-100 delay-200' : 'opacity-0'}
+          `}
+          muted
+          playsInline
+          src={videoUrl}
+          ref={videoRef}
+        />
       </div>
       <div className="flex gap-2">
         <div
@@ -80,19 +117,19 @@ const VideoGridItem: React.FC<VideoGridItemProps> = ({
             height="100"
             src={channel.profileUrl}
             alt={channel.name}
-            className="w-12 h-12 rounded-full"
+            className="w-12 h-12 rounded-full cursor-pointer"
           />
         </div>
         <div className="flex flex-col">
           <div
             onClick={() => router.push(`/whatch?v=${id}`)}
-            className="font-bold"
+            className="font-bold cursor-pointer"
           >
             {title}
           </div>
           <div
             onClick={() => router.push(`/@${channel.id}`)}
-            className="text-sm text-secondary-text"
+            className="text-sm text-secondary-text cursor-pointer hover:text-secondary-dark-hover"
           >
             {channel.name}
           </div>
