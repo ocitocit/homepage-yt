beeedea4ad6cbd140db74c95f7cfc18de2b9f137
Abdul Rosyid <ocritocrit@gmail.com>
2023-10-29 07:43:48 +0700

    feat: smallsidebar

--- components/sidebar/SideBar.tsx
+++ components/sidebar/SideBar.tsx
@@ -2,21 +2,9 @@ import SmallSidebar from './SmallSidebar';
 
 const SideBar = () => {
   return (
-    <aside
-      className="
-      top-0
-      sticky
-      overflow-y-auto
-      scrollbar-hidden
-      pb-4
-      ml-1
-      flex
-      flex-col
-      lg:hidden
-      "
-    >
+    <>
       <SmallSidebar />
-    </aside>
+    </>
   );
 };
 
--- components/sidebar/SmallSidebar.tsx
+++ components/sidebar/SmallSidebar.tsx
@@ -1,22 +1,34 @@
-import { Home } from 'lucide-react';
+import { Clapperboard, Home, Library, Repeat } from 'lucide-react';
 import ContainerSmallSidebar from './ContainerSmallSidebar';
 
 const SmallSidebar = () => {
   return (
-    <>
+    <aside
+      className="
+      top-0
+      sticky
+      overflow-y-auto
+      scrollbar-hidden
+      pb-4
+      ml-1
+      flex
+      flex-col
+      lg:hidden
+      "
+    >
       <ContainerSmallSidebar title="Home" url="/">
         <Home />
       </ContainerSmallSidebar>
-      <ContainerSmallSidebar title="Home" url="/">
-        <Home />
+      <ContainerSmallSidebar title="Short" url="/shorts">
+        <Repeat />
       </ContainerSmallSidebar>
-      <ContainerSmallSidebar title="Home" url="/">
-        <Home />
+      <ContainerSmallSidebar title="Subscriptions" url="/subscriptions">
+        <Clapperboard />
       </ContainerSmallSidebar>
-      <ContainerSmallSidebar title="Home" url="/">
-        <Home />
+      <ContainerSmallSidebar title="Library" url="/library">
+        <Library />
       </ContainerSmallSidebar>
-    </>
+    </aside>
   );
 };
 
